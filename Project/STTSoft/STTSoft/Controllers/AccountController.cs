4ee2bd78071fc5d8c6c79c578b8e8575d8ee8b92
sinhcvse60692@fpt.edu.vn <sinhcvse60692@fpt.edu.vn@18173ba8-0869-0ab4-a3d9-622385fa8388>
2013-10-21 11:33:32 +0000

    Commit giao dien

--- Project/STTSoft/STTSoft/Controllers/AccountController.cs
+++ Project/STTSoft/STTSoft/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using STTSoft.Models;
 
 namespace STTSoft.Controllers
 {
@@ -10,15 +11,58 @@ namespace STTSoft.Controllers
     {
         //
         // GET: /Account/
+        STTSoftDataContext db = new STTSoftDataContext();
 
+        [HttpGet]
         public ActionResult Login()
+        {
+            ViewBag.status = (string)TempData["error"];
+            return PartialView("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Select the input usename in textbox at login form
+            username = Request.Params["txtUsername"];
+            // Select the input password in textbox at login form
+            password = Request.Params["txtPassword"];
+            try
+            {
+                var account = (from acc in db.Accounts
+                               where acc.AccName.Equals(username) && acc.AccPass.Equals(password)
+                               select acc).SingleOrDefault();
+
+                //Kiểm tra username và password trong database
+                if (account != null)
+                {
+                    Session["username"] = username;
+                    Session["role"] = account.AccRole;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["error"] = "Tên đăng nhập or mật khẩu của bạn không đúng, Vui lòng nhập lại";
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+            catch (ArgumentNullException argumentNullException)
+            {
+                throw argumentNullException;
+            }
+        }
+
+        //Đăng xuất
+        [HttpGet]
+        public ActionResult Logout()
         {
             return View();
         }
 
+
         public ActionResult Register()
         {
-            return View();
+            return PartialView();
         }
 
         public ActionResult ForgetPass()
